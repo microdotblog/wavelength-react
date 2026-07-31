f1025e11003d08fb224ece2ac74707e58fe5aed7
Manton Reece <manton@me.com>
2026-07-31 08:37:05 -0500

    Fixed stdint include.

--- modules/wavelength-mp3/android/src/main/cpp/lame/config.h
+++ modules/wavelength-mp3/android/src/main/cpp/lame/config.h
@@ -6,6 +6,8 @@
 #ifndef WAVELENGTH_LAME_CONFIG_H
 #define WAVELENGTH_LAME_CONFIG_H
 
+#include <stdint.h>
+
 #ifndef HAVE_IEEE754_FLOAT32_T
 typedef float ieee754_float32_t;
 #endif
@@ -16,14 +18,6 @@ typedef double ieee754_float64_t;
 
 #ifndef HAVE_IEEE854_FLOAT80_T
 typedef long double ieee854_float80_t;
-#endif
-
-#ifndef HAVE_INT16_T
-typedef short int16_t;
-#endif
-
-#ifndef HAVE_UINT16_T
-typedef unsigned short uint16_t;
 #endif
 
 #endif
